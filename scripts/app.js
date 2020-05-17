ce77802337efcaf9772c108c5d93e47a242b9201
Justinas Kairys <info@ibnz.co.uk>
2020-05-17 11:16:54 +0100

    Added eye movement

--- scripts/app.js
+++ scripts/app.js
@@ -1,64 +1,68 @@
 const cordinates = document.getElementById('cordinates');
 const areaL = document.getElementById('area-l');
-const head = document.getElementById('chead');
+const head = document.getElementById('chead-svg');
+const eye = document.getElementById('Layer_2_1_');
+// areaL.style.cursor = `copy`;
 
 showCordinates = (cordX, cordY) => {
   cordinates.innerHTML = `possition x-${cordX}, possition y-${cordY}, position divided-${Math.round(
     cordY / 10
   )}`;
-
-  // if (Math.round(cordY/10 >50 ) {
-  //     head.style.transform = "rotate(15deg)";
-  // }
-
-//   if (Math.round(cordY / 10) < 50) {
-//     console.log(Math.round(cordY / 10));
-//   } else if (Math.round(cordY / 10) > 50){
- //   console.log(Math.round(cordY / 10));
-//   } else 
-  
   if (cordY < 100) {
     head.style.transform = 'rotate(15deg)';
+    eye.style.transform = 'rotate(-3deg)';
   } else if (cordY < 200) {
     head.style.transform = 'rotate(10deg)';
+    eye.style.transform = 'rotate(-2deg)';
   } else if (cordY < 300) {
     head.style.transform = 'rotate(5deg)';
+    eye.style.transform = 'rotate(-1deg)';
   } else if (cordY < 400) {
     head.style.transform = 'rotate(0deg)';
+    eye.style.transform = 'rotate(-1deg)';
   } else if (cordY < 500) {
     head.style.transform = 'rotate(-2deg)';
+    eye.style.transform = 'rotate(-0.5deg)';
   } else if (cordY < 600) {
     head.style.transform = 'rotate(-5deg)';
+    eye.style.transform = 'rotate(-0.2deg)';
   } else if (cordY < 700) {
     head.style.transform = 'rotate(-10deg)';
+    eye.style.transform = 'rotate(0deg)';
   } else if (cordY < 800) {
     head.style.transform = 'rotate(-15deg)';
+    eye.style.transform = 'rotate(1deg)';
   } else if (cordY < 900) {
     head.style.transform = 'rotate(-20deg)';
+    eye.style.transform = 'rotate(2deg)';
   } else if (cordY < 1000) {
     head.style.transform = 'rotate(-25deg)';
+    eye.style.transform = 'rotate(1deg)';
+  }
+};
+
+moveEye = (cordX, cordY) => {
+  if (cordX < 30) {
+    eye.style.transform = 'translate(-3px, 0px)';
+  } else if (cordX < 100) {
+    eye.style.transform = 'translate(-2.5px, 0px)';
+  } else if (cordX < 150) {
+    eye.style.transform = 'translate(-2px, 0px)';
+  } else if (cordX < 200) {
+    eye.style.transform = 'translate(-1.5px, 0px)';
+  } else if (cordX < 300) {
+    eye.style.transform = 'translate(-1px, 0px)';
   }
 };
 
 generateCordinates = (event) => {
-  //   console.log(event.clientX);
   showCordinates(event.clientX, event.clientY);
+  moveEye(event.clientX, event.clientY);
 };
 
-hideHead = () => {
-  if (head.style.display === 'none') {
-    // head.style.display = "block";
-  } else {
-    // head.style.display = "none";
-    deg = rotated ? 0 : 66;
+// hideHead = (cordX, cordY) => {
 
-    head.style.webkitTransform = 'rotate(' + deg + 'deg)';
-    head.style.mozTransform = 'rotate(' + deg + 'deg)';
-    head.style.msTransform = 'rotate(' + deg + 'deg)';
-    head.style.oTransform = 'rotate(' + deg + 'deg)';
-    head.style.transform = 'rotate(' + deg + 'deg)';
-  }
-};
+// };
 
 areaL.addEventListener('mousemove', generateCordinates);
-areaL.addEventListener('click', hideHead);
+// areaL.addEventListener('mouseleave', hideHead);
